fa815938640dde178df32b7cd3110a78ca09d8a3
fathan1208 <fathannur595@gmail.com>
2021-10-07 21:28:59 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+    public static void main(String[] args){
+	    System.out.println("Nama : Fathan");
+		System.out.println("NIM : 123456");
+		System.out.println("Kelas : 44 xx");
+	}
+}
